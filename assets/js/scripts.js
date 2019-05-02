b46da11fbf6b562dd5a74feafd1c433e6269396c
QiyanWang <chicowang16@gmail.com>
2019-05-01 19:20:50 -0700

    google map

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -327,7 +327,7 @@ jQuery(function ($) {
     // -------------------------------------------------------------
 
     (function () {
-        var myLatlng = new google.maps.LatLng(41.372641, -74.687387);
+        var myLatlng = new google.maps.LatLng(37.338207, -121.886330);
 
             var styles = [
                 {
